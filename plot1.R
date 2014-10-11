64d776dfa44ea37d5ef63968b71c489931fec043
Alvin Goh <alvinaloy@hotmail.com>
2014-10-12 01:40:36 +0800

    Committed

--- plot1.R
+++ plot1.R
@@ -2,12 +2,12 @@
 data <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";", colClass = "character")
 
 ##Subset out the required data
-data_subset <- data[(data$Date == "1/2/2007" | data$Date == "2/2/2007"), ]
+data_sub <- data[(data$Date == "1/2/2007" | data$Date == "2/2/2007"), ]
 
 ##Change numbers to numeric
-data_subset$Global_active_power <- as.numeric(data_subset$Global_active_power)
+data_sub$Global_active_power <- as.numeric(data_sub$Global_active_power)
 
 ##Create plot1.png
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
-hist(data_subset$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+hist(data_sub$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
